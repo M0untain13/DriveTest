e35f679ed4a2b37021f774d74443ce361ae67b77
Парамонов Арсений <paramosha228@gmail.com>
2023-03-21 09:25:12 +0500

    Реализовал получение данных из класса DResults

--- Приложение/Classes/DResults.cs
+++ Приложение/Classes/DResults.cs
@@ -8,41 +8,155 @@ using System.Threading.Tasks;
 
 namespace Приложение.Classes
 {
-    //TODO: нужно заполнить классы
+    //TODO: нужно организовать проведения тестирования, для того чтобы здесь определиться с заполнением результатов
     public class DResults
     {
-        private ObservableCollection<AbstractDResultsAnswer> _answers = new();
-        public ObservableCollection<AbstractDResultsAnswer> Answers { get { return _answers; } }
+        private readonly IEnumerable<AbstractDResultsAnswer> _answers = Enumerable.Empty<AbstractDResultsAnswer>();
+        public IEnumerable<AbstractDResultsAnswer> Answers { get { return _answers; } }
     }
+    /// <summary>
+    /// На вывод: Имя, баллы * процент_корректности, варианты ответов при необходимости, данный ответ
+    /// </summary>
     public abstract class AbstractDResultsAnswer
     {
-        public string name;
-        public double score;
+        private readonly string _name;
+        private readonly double _score;
+        /// <summary>
+        /// Название вопроса
+        /// </summary>
+        public string Name { get { return _name; } }
+        /// <summary>
+        /// Кол-во баллов за ответ
+        /// </summary>
+        public double Score { get { return _score * Сorrectness; } }
+        /// <summary>
+        /// Процент корректности данного ответа от 0 до 1
+        /// </summary>
+        protected virtual double Сorrectness => 0;
+        /// <summary>
+        /// Предполагается список вариантов ответа
+        /// </summary>
+        public virtual IEnumerable<string> ArrayOfString_01 { get { return Enumerable.Empty<string>();} }
+        /// <summary>
+        /// Предполагается список ответов данных пользователем
+        /// </summary>
+        public virtual IEnumerable<string> ArrayOfString_02 { get { return Enumerable.Empty<string>(); } }
     }
+    /// <summary>
+    /// Несколько вариантов и все верные
+    /// В ответе может быть дано несколько ответов, могут быть неверными и верными
+    /// На вывод: только данный ответ
+    /// </summary>
     public class DResultsAnswerOpen : AbstractDResultsAnswer
     {
-        public List<string> correctAnswer = new();
-        public string answerFromTesting = "";
+        private readonly HashSet<string> _correctAnswers = new();
+        private readonly string _answerFromTesting = "";
+        protected override double Сorrectness 
+        { 
+            get 
+            {
+                var arrayOfAnswerFromTesting = _answerFromTesting.Split();
+                var countOfErrors = arrayOfAnswerFromTesting.Length - (from answer in arrayOfAnswerFromTesting
+                                                                      where _correctAnswers.Contains(answer)
+                                                                      select answer).Count();
+                var countOfCorrectAnswers = arrayOfAnswerFromTesting.Length-countOfErrors;
+                //Объяснение данной формулы есть на гитхабе в roadmap в выполненной задаче "Классы внутренней логики"
+                return countOfCorrectAnswers/ _correctAnswers.Count * (1 - countOfErrors / _correctAnswers.Count); 
+            } 
+        }
+        /// <summary>
+        /// Возвращает пустой список, т.к. все ответы из списка являются корректными, а значит их палить нельзя
+        /// </summary>
+        public override IEnumerable<string> ArrayOfString_01 => base.ArrayOfString_01;
+        public override IEnumerable<string> ArrayOfString_02 => _answerFromTesting.Split();
     }
+    /// <summary>
+    /// Несколько вариантов и один верный
+    /// В ответе может быть дан только один ответ
+    /// На вывод: Варианты и данный ответ
+    /// </summary>
     public class DResultsAnswerSelectiveOne : AbstractDResultsAnswer
     {
-        public List<string> answers = new();
-        public string studentAnswer;
-        bool correct;
+        private readonly List<string> _answers = new();
+        private readonly string _receivedAnswer = "";
+        private readonly bool _correct;
+        protected override double Сorrectness
+        {
+            get
+            {
+                if (_correct) return 1;
+                return 0;
+            }
+        }
+        public override IEnumerable<string> ArrayOfString_01 => _answers;
+        public override IEnumerable<string> ArrayOfString_02 { get { yield return _receivedAnswer; }
+        }
     }
+    /// <summary>
+    /// Несколько вариантов и несколько верных
+    /// В ответе может быть дано несколько ответов
+    /// На вывод: Варианты и данные ответы
+    /// </summary>
     public class DResultsAnswerSelectiveMultiple : AbstractDResultsAnswer
     {
-        public List<string> answers = new();
-        public List<string> correctAnswer = new();
-        bool correct;
+        private readonly HashSet<string> _answers = new();
+        private readonly HashSet<string> _correctAnswers = new();
+        private readonly HashSet<string> _receivedAnswers = new();
+        protected override double Сorrectness 
+        {
+            get
+            {
+                var countOfCorrectAnswers = (from answer in _receivedAnswers
+                                             where _correctAnswers.Contains(answer)
+                                             select answer).Count();
+                var countOfErrors = _receivedAnswers.Count() - countOfCorrectAnswers;
+
+                //Объяснение данной формулы есть на гитхабе в roadmap в выполненной задаче "Классы внутренней логики"
+                return countOfCorrectAnswers / _correctAnswers.Count * (1 - countOfErrors / _answers.Count);
+            }
+        }
+        public override IEnumerable<string> ArrayOfString_01 => _answers;
+        public override IEnumerable<string> ArrayOfString_02 => _receivedAnswers;
     }
+    /// <summary>
+    /// Пары ответов
+    /// На вывод: Данная пара ответов
+    /// </summary>
     public class DResultsAnswerMatchingSearch : AbstractDResultsAnswer
     {
-        Dictionary<string, string> correctAnswers = new Dictionary<string, string>();
-        Dictionary<string, string> studentAnswers = new Dictionary<string, string>();
+        private readonly Dictionary<string, string> _correctAnswers = new();
+        private readonly Dictionary<string, string> _receivedAnswers = new();
+        protected override double Сorrectness
+        {
+            get
+            {
+                var countOfCorrectAnswers = (from answer in _receivedAnswers
+                                             where _correctAnswers[answer.Key] == _receivedAnswers[answer.Key]
+                                             select answer).Count();
+                var countOfErrors = _receivedAnswers.Count() - countOfCorrectAnswers;
+
+                //Объяснение данной формулы есть на гитхабе в roadmap в выполненной задаче "Классы внутренней логики"
+                return countOfCorrectAnswers / _correctAnswers.Count * (1 - countOfErrors / _correctAnswers.Count);
+            }
+        }
+        /// <summary>
+        /// Возвращаем первый список данных ответов, которые являются парами ответам из второго списка
+        /// </summary>
+        public override IEnumerable<string> ArrayOfString_01 => _receivedAnswers.Keys;
+        /// <summary>
+        /// Возвращаем второй список данных ответов, которые являются парами ответам из первого списка
+        /// </summary>
+        public override IEnumerable<string> ArrayOfString_02 => _receivedAnswers.Values;
     }
+    /// <summary>
+    /// Просто строка
+    /// На вывод: Данная строка
+    /// </summary>
     public class DResultsAnswerDataInput : AbstractDResultsAnswer
     {
-        public string info;
+        private readonly string _info;
+        protected override double Сorrectness => base.Сorrectness;
+        public override IEnumerable<string> ArrayOfString_01 { get { yield return _info; } }
+        public override IEnumerable<string> ArrayOfString_02 => base.ArrayOfString_02;
     }
 }
